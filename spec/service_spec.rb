e3324499472d66679f9f060803ded09f9beafc19
Ivan Karl <ivan6258@gmail.com>
2014-04-09 13:29:04 -0300

    first commit

---
+++ spec/service_spec.rb
@@ -0,0 +1,68 @@
+require File.dirname(__FILE__) + '/../service'
+require 'spec'
+require 'spec/interop/test'
+require 'rack/test'
+
+set :environment, :test
+Test::Unit::TestCase.send :include, Rack::Test::Methods
+def app
+  Sinatra::Application
+end
+describe "service" do
+  before(:each) do
+    User.delete_all
+  end
+  describe "GET on /api/v1/users/:id" do
+    before(:each) do
+      User.create(
+        :name     => "paul",
+        :email    => "paul@pauldix.net",
+        :password => "strongpass",
+        :bio      => "rubyist"
+      )
+    end
+    it "should return a user by name" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes["name"].should == "paul"
+    end
+    it "should return a user with an email" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes["email"].should == "paul@pauldix.net"
+    end
+    it "should not return a user's password" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes.should_not have_key("password")
+    end
+    it "should return a user with a bio" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes["bio"].should == "rubyist"
+    end
+    it "should return a 404 for a user that doesn't exist" do
+      get '/api/v1/users/foo'
+      last_response.status.should == 404
+    end
+  end
+  describe "POST on /api/v1/users" do
+    it "should create a user" do
+      post '/api/v1/users', {
+          :name     => "trotter",
+          :email    => "no spam",
+          :password => "whatever",
+          :bio      => "southern belle"}.to_json
+        last_response.should be_ok
+        get '/api/v1/users/trotter'
+        attributes = JSON.parse(last_response.body)
+        attributes["name"].should  == "trotter"
+        attributes["email"].should == "no spam"
+        attributes["bio"].should   == "southern belle"
+    end
+  end
+end
